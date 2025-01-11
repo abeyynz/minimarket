aa62516959173fc59d1190580f8bd726859e2da8
abiyyatun nazihah <abiyyatunnazihah@gmail.com>
2025-01-11 13:01:17 +0700

    Logo Jayusmart

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-blue-500 dark:text-gray-100">
-                    <h1>{{ __('Report ').$store->name }}</h1>
+                    {{-- <h1>{{ __('Report ').$store->name }}</h1> --}}
                 </div>
             </div>
 
